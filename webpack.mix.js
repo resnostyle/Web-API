8a39e7a4789e8a3cbcfd5d9ecd69f8e7affeabab
zetas <ddv@qubitlogic.net>
2017-10-10 03:53:10 -0400

    Not currently needing an image directory, comment it out so it builds properly.

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,7 +13,7 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
-    .copyDirectory('resources/assets/images', 'public/images')
+    //.copyDirectory('resources/assets/images', 'public/images')
     .version()
     .browserSync('laranzedb.app');
 
