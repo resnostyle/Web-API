0fb5d749a5101807f84039874c86d274b6dd553a
zetas <ddv@qubitlogic.net>
2017-10-10 22:58:32 -0400

    Disabling notifications on every page load because it's annoying.

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,6 +15,9 @@ mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
     //.copyDirectory('resources/assets/images', 'public/images')
     .version()
-    .browserSync('laranzedb.app');
+    .browserSync({
+        'proxy': 'laranzedb.app',
+        'notify': false
+    });
 
 
