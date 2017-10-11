9dc749d7fc21c0b6305e0251cc550926c6affaeb
zetas <ddv@qubitlogic.net>
2017-10-11 09:40:49 -0400

    Refactor temporary view to be cleaner and include group display.

--- resources/views/category/browse.blade.php
+++ resources/views/category/browse.blade.php
@@ -1,18 +1,28 @@
 @extends('layout.app')
 
 @section('content')
+    <h1>Releases for @if ($category->parent != '')
+            {{$category->parent->title}}->
+        @endif
+        {{ $category->title }}</h1>
+    <h3>Total: {{ count($releases) }}</h3>
+
     @forelse($releases as $release)
-        <ul>
-            <li>{{ $release->name }}</li>
-            <li>{{ $release->guid }}</li>
-            <li>
-                @if ($release->category->parent != '')
-                    {{$release->category->parent->title}}->
-                @endif
-                {{ $release->category->title }}</li>
-        </ul>
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">{{ $release->name }}</h4>
+                <h6 class="card-subtitle mb-2 text-muted">{{ $release->guid }}</h6>
+                <ul class="list-group list-group-flush">
+                    @forelse($release->groups as $group)
+                        <li class="list-group-item">Group: {{ $group->name }}</li>
+                    @empty
+                        <li class="list-group-item">No group associated.</li>
+                    @endforelse
+                </ul>
+            </div>
+        </div>
     @empty
         <p>No releases for this category</p>
     @endforelse
 
-    @endsection
+@endsection
