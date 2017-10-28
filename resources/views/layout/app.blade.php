9165b51d706dd191d0492a00b87ec2e8c110698e
zetas <ddv@qubitlogic.net>
2017-10-28 12:58:30 -0400

    Add logout button with proper form submit method.

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -56,6 +56,17 @@
                             <a class="nav-link" href="#">No Categories Loaded</a>
                         </li>
                     @endforelse
+                    <li>
+                        <a class="nav-link" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
+                    </li>
                     @endguest
         </ul>
         <form class="form-inline my-2 my-lg-0">
