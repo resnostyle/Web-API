d40b67e9fd55e1c72ee67401ee76eb6c4367b291
zetas <ddv@qubitlogic.net>
2017-10-31 21:34:40 -0400

    Update db field to fit with project settings of using underscores for multi-word field names

--- database/migrations/2017_10_28_203915_add_apikey_to_users_table.php
+++ database/migrations/2017_10_28_203915_add_apikey_to_users_table.php
@@ -14,7 +14,7 @@ class AddApikeyToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('apikey')->nullable();
+            $table->text('api_key');
         });
     }
 
@@ -26,7 +26,7 @@ class AddApikeyToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(['apikey']);
+            $table->dropColumn(['api_key']);
         });
     }
 }
