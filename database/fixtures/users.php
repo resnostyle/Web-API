c22b97347103b755657d7774754a1319e33542e1
zetas <ddv@qubitlogic.net>
2017-11-01 02:10:10 -0400

    Updating apikey to api_key in fixture data to match with project guidelines

--- database/fixtures/users.php
+++ database/fixtures/users.php
@@ -5,12 +5,12 @@ return [
         'username' => 'admin',
         'email' => encrypt('admin@example.com'),
         'password' => bcrypt('secret'),
-        'apikey'=>'1a64038e383ec2aab5ca451cb56e669a'
+        'api_key'=>'1a64038e383ec2aab5ca451cb56e669a'
     ],
     [
         'username' => 'basicuser',
         'email' => encrypt('basic@example.com'),
         'password' => bcrypt('secret'),
-        'apikey'=>'1a64038e383ec2aab5ca451cb56e669a'
+        'api_key'=>'1a64038e383ec2aab5ca451cb56e669a'
     ]
 ];
